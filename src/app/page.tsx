534295042bae705cd3e72448dd9288b0bc630268
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-06-14 16:41:24 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { columns } from "@/components/user/columns";
+import DataTable from "@/components/user/DataTable";
+import { getUsers } from "@/services/userService";
+import LogoutButton from "@/components/user/LogoutButton";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+export default async function Home() {
+  const users = await getUsers();
+  console.log(users);
 
-export default Home;
+  return (
+    <div className="flex-col flex-1 hidden h-full p-8 space-y-8 md:flex">
+      <div className="flex items-center justify-between space-y-2">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">
+            Welcome to LIKELION!
+          </h2>
+          <p className="text-muted-foreground">
+            Here&apos;s a list of users who registered courses!
+          </p>
+        </div>
+        <div>
+          <LogoutButton />
+        </div>
+      </div>
+
+      {!users ? (
+        <div>No users found!</div>
+      ) : (
+        <DataTable data={users} columns={columns} />
+      )}
+    </div>
+  );
+}
